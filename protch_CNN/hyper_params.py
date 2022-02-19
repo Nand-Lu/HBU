520601f0eb1530b5531bf27ba2da3f37f8c43c67
Nand-Lu <70938007+Nand-Lu@users.noreply.github.com>
2022-02-19 20:18:05 +0800

    Add files via upload

--- protch_CNN/hyper_params.py
+++ protch_CNN/hyper_params.py
@@ -4,8 +4,9 @@
 
 hyper_params = {
     "learning_rate": 0.001,
-    "epochs": 30,
-    "batch_size": 8,
+    "epochs": 10,
+    "batch_size": 4,
     "num_classes":2,
     "input_size":128,
+    "pre_modelpath":r".\resnet50-19c8e357.pth",
 }
