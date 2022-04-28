250155b8dd4edeec716cb0b3504368bffa971227
Nand <70938007+Nand-Lu@users.noreply.github.com>
2022-04-28 10:39:21 +0800

    Update Inception_resnet_v2.py

--- protch_CNN/model/Inception_resnet_v2.py
+++ protch_CNN/model/Inception_resnet_v2.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from hyper_params  import  hyper_params
+
 
 class conv3x3(nn.Module):
     def __init__(self, in_planes, out_channels, stride=1, padding=0):
@@ -42,11 +42,12 @@ class StemV2(nn.Module):
         self.conv5 = conv1x1(in_planes=160, out_channels=64,  stride=1, padding=1)
         self.conv6 = conv3x3(in_planes=64, out_channels=96, stride=1, padding=0)
         self.conv7 = conv1x1(in_planes=160, out_channels=64,  stride=1, padding=1)
-        self.conv8 = nn.Conv2d(in_channels=64, out_channels=64, stride=1, kernel_size=(1, 7), padding=(0, 3))
-        self.conv9 = nn.Conv2d(in_channels=64, out_channels=64, stride=1, kernel_size=(7, 1), padding=(3, 0))
+        self.conv8 = nn.Conv2d(in_channels=64, out_channels=64, stride=1, kernel_size=(7, 1), padding=(3, 0))
+        self.conv9 = nn.Conv2d(in_channels=64, out_channels=64, stride=1, kernel_size=(1, 7), padding=(0, 3))
+
         self.conv10 = conv3x3(in_planes=64, out_channels=96, stride=1, padding=0)
-        self.conv11 = conv3x3(in_planes=96, out_channels=192, stride=1, padding=0)
-        self.maxpool2 = nn.MaxPool2d(kernel_size=1, stride=2, padding=0)
+        self.conv11 = conv3x3(in_planes=192, out_channels=192, stride=2, padding=0)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
         self.relu = nn.ReLU()
 
     def forward(self, x):
@@ -70,27 +71,25 @@ class StemV2(nn.Module):
         x = torch.cat([x1, x2], dim=1)
 
 
-
-
         return x
 
 class Inception_ResNet_A(nn.Module):
     def __init__(self, input,  scale=0.3):
         super(Inception_ResNet_A, self).__init__()
-        self.conv1 = conv1x1(in_planes =input,out_channels=32,stride=1, padding=1)
+        self.conv1 = conv1x1(in_planes =input,out_channels=32,stride=1, padding=0)
 
         self.conv2 = conv3x3(in_planes=32, out_channels=32, stride=1, padding=1)
 
         self.conv3 = conv3x3(in_planes=32, out_channels=48, stride=1, padding=1)
         self.conv4 = conv3x3(in_planes=48, out_channels=64, stride=1, padding=1)
 
-        self.line =  nn.Conv2d(128, 384, 1, stride=1, padding=1, bias=True)
+        self.line =  nn.Conv2d(128, 384, 1, stride=1, padding=0, bias=True)
         self.scale = scale
 
         self.relu = nn.ReLU()
 
     def forward(self, x):
-
+        # print("x", x.shape)
         c1 = self.conv1(x)
         # print("c1",c1.shape)
         c2 = self.conv1(x)
@@ -103,8 +102,8 @@ class Inception_ResNet_A(nn.Module):
         # print("c3_1", c3_1.shape)
         c3_2 = self.conv4(c3_1)
         # print("c3_2", c3_2.shape)
-        cat = torch.cat([c1, c2_1, c3_2],dim=1)#torch.Size([4, 96, 15, 15])
-        # print("x",x.shape)
+        cat = torch.cat([c1, c2_1, c3_2],dim=1)
+        # print("cat",cat.shape)
 
         line = self.line(cat)
         # print("line",line.shape)
@@ -116,18 +115,18 @@ class Inception_ResNet_A(nn.Module):
 class Inception_ResNet_B(nn.Module):
     def __init__(self, input, scale=0.3):
         super(Inception_ResNet_B, self).__init__()
-        self.conv1 = conv1x1(in_planes =input,out_channels=192,stride=1, padding=1)
-        self.conv2 = conv1x1(in_planes =input,out_channels=128,stride=1, padding=1)
+        self.conv1 = conv1x1(in_planes =input,out_channels=192,stride=1, padding=0)
+        self.conv2 = conv1x1(in_planes =input,out_channels=128,stride=1, padding=0)
 
         self.conv1x7 = nn.Conv2d(in_channels=128,out_channels=160,kernel_size=(1,7), padding=(0,3))
         self.conv7x1 = nn.Conv2d(in_channels=160, out_channels=192,kernel_size=(7,1), padding=(3,0))
-        self.line = nn.Conv2d(384, 1154, 1, stride=1, padding=1, bias=True)
+        self.line = nn.Conv2d(384, 1152, 1, stride=1, padding=0, bias=True)
         self.scale = scale
 
         self.relu = nn.ReLU()
 
     def forward(self, x):
-
+        # print("x", x.shape)
         c1 = self.conv1(x)
         # print("c1",c1.shape)
         c2 = self.conv2(x)
@@ -154,10 +153,10 @@ class Inception_ResNet_B(nn.Module):
 class Inception_ResNet_C(nn.Module):
     def __init__(self, input, scale=0.3):
         super(Inception_ResNet_C, self).__init__()
-        self.conv1 = conv1x1(in_planes=input, out_channels=192, stride=1, padding=1)
+        self.conv1 = conv1x1(in_planes=input, out_channels=192, stride=1, padding=0)
         self.conv1x3 = nn.Conv2d(in_channels=192, out_channels=224, kernel_size=(1, 3), padding=(0,1))
         self.conv3x1 = nn.Conv2d(in_channels=224, out_channels=256, kernel_size=(3, 1), padding=(1,0))
-        self.line = nn.Conv2d(448, 2948, 1, stride=1, padding=1, bias=True)
+        self.line = nn.Conv2d(448, 2048, 1, stride=1, padding=0, bias=True)
 
         self.relu = nn.ReLU()
         self.scale = scale
@@ -185,7 +184,7 @@ class Inception_ResNet_C(nn.Module):
         return out
 
 class Reduction_A(nn.Module):
-    def __init__(self, input, n=384, k=256, l=256, m=256, scale=0.3):
+    def __init__(self, input, n=384, k=256, l=256, m=384, scale=0.3):
         super(Reduction_A, self).__init__()
         self.maxpool = nn.MaxPool2d(kernel_size=3,  stride=2, padding=0)
         self.conv1 = conv3x3(in_planes=input, out_channels=n, stride=2,padding=0)
@@ -213,11 +212,11 @@ class Reduction_A(nn.Module):
 class Reduction_B(nn.Module):
     def __init__(self, input):
         super(Reduction_B, self).__init__()
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.conv1 = conv1x1(in_planes=input,  out_channels=256, padding=1)
-        self.conv2 = conv3x3(in_planes=256,  out_channels=384, stride=2, padding=0)
+        self.conv2 = conv3x3(in_planes=256,  out_channels=288, stride=2, padding=0)
         self.conv3 = conv3x3(in_planes=256,  out_channels=288, stride=2, padding=0)
-        self.conv4 = conv3x3(in_planes=256,  out_channels=288,  padding=1)
+        self.conv4 = conv3x3(in_planes=256,  out_channels=288, stride=1, padding=1)
         self.conv5 = conv3x3(in_planes=288,  out_channels=320, stride=2, padding=0)
     def forward(self, x):
         c1 = self.maxpool(x)
@@ -229,7 +228,7 @@ class Reduction_B(nn.Module):
         c4 = self.conv1(x)
         # print("c4", c4.shape)
         c2_1 = self.conv2(c2)
-        # print("cc2_1", c2_1.shape)
+        # print("c2_1", c2_1.shape)
         c3_1 = self.conv3(c3)
         # print("c3_1", c3_1.shape)
         c4_1 = self.conv4(c4)
@@ -247,20 +246,20 @@ class Inception_ResNet_v2(nn.Module):
         blocks = []
         blocks.append(StemV2(in_planes=3))
         for _ in range(5):
-            blocks.append(Inception_ResNet_A(input=288))
+            blocks.append(Inception_ResNet_A(input=384))
         blocks.append(Reduction_A(input=384))
         for _ in range(10):
-            blocks.append(Inception_ResNet_B(input=1024))
-        blocks.append(Reduction_B(input=1154))
+            blocks.append(Inception_ResNet_B(input=1152))
+        blocks.append(Reduction_B(input=1152))
         for _ in range(10):
-            blocks.append(Inception_ResNet_C(input=2146))
+            blocks.append(Inception_ResNet_C(input=2048))
 
         self.features = nn.Sequential(*blocks)
 
         self.avepool = nn.AvgPool2d(kernel_size=3)
 
         self.dropout = nn.Dropout(p=0.2)
-        self.linear = nn.Linear(2146, classes)
+        self.linear = nn.Linear(2048, classes)
 
     def forward(self, x):
         x = self.features(x)
@@ -274,3 +273,9 @@ class Inception_ResNet_v2(nn.Module):
 
         return  x
 
+if __name__ == '__main__':
+    x = torch.randn(2, 3, 224, 224)
+    net = Inception_ResNet_v2()
+    out = net(x)
+    print('out.shape: ', out.shape)
+    print(out)
